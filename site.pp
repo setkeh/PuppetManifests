4cb968e11add1dba108f87ec2f0d7647134a1b4e
Setkeh <setkeh@gmail.com>
2015-04-05 05:09:15 +1000

    Trying something else

--- site.pp
+++ site.pp
@@ -6,39 +6,39 @@ node '841734fa-cd43-4516-baa9-75e7a875d82e' {
 }
 node 'dns.setkeh.local' {
   #puppet code
-  include dns::server
 
-    # Forwarders
-    dns::server::options { '/etc/bind/named.conf.options':
-      forwarders => [ '8.8.8.8', '8.8.4.4' ]
-    }
+  include dns::server
+dns::zone { 'setkeh.local':
+ soa         => 'ns1.setkeh.local',
+ soa_email   => 'setkeh@gmail.com',
+ nameservers => ['ns1.setkeh.local']
+}
+dns::zone { '1.168.192.IN-ADDR.ARPA':
+ soa         => 'ns1.setkeh.local',
+ soa_email   => 'setkeh@gmail.com',
+ nameservers => ['ns1.example.com']
+}
+dns::record::a { 'ns1':
+ zone => 'setkeh.local',
+ data => ['192.168.1.254'],
+ ptr  => true;
+}
 
     # Forward Zone
-    dns::zone { 'setkeh.local':
-      soa         => 'ns1.setkeh.local',
-      soa_email   => 'setkeh@gmail.com',
-      nameservers => ['ns1']
-    }
+    #dns::zone { 'setkeh.local':
+    #  soa         => 'ns1.setkeh.local',
+    #  soa_email   => 'setkeh@gmail.com',
+    #  nameservers => ['ns1']
+    #}
 
     # Reverse Zone
-    dns::zone { '1.168.192.IN-ADDR.ARPA':
-      soa         => 'ns1.setkeh.local',
-      soa_email   => 'setkeh@gmail.com',
-      nameservers => ['ns1']
-    }
+    #dns::zone { '1.168.192.IN-ADDR.ARPA':
+    #  soa         => 'ns1.setkeh.local',
+    #  soa_email   => 'setkeh@gmail.com',
+    #  nameservers => ['ns1']
+    #}
 
-    # A Records:
-    dns::record::a {
-      'dns':
-        zone => 'setkeh.local',
-        data => ['192.168.1.254'],
-        ptr  => true;
-      'test':
-        zone => 'setkeh.local',
-        data => ['192.168.1.12'],
-        ptr  => true;
-    }
-}
+  }
 
 # Site Wide Config
 import 'site/*.pp'
