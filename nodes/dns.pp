20b1c1112ddf9e5a6ebaa797e5148c4008377ede
Setkeh <setkeh@gmail.com>
2015-04-05 04:35:23 +1000

    Added initial DNS config to see what breaks.

---
+++ nodes/dns.pp
@@ -0,0 +1,32 @@
+include dns::server
+
+  # Forwarders
+  dns::server::options { '/etc/bind/named.conf.options':
+    forwarders => [ '8.8.8.8', '8.8.4.4' ]
+  }
+
+  # Forward Zone
+  dns::zone { 'setkeh.local':
+    soa         => 'ns1.setkeh.local',
+    soa_email   => 'setkeh@gmail.com',
+    nameservers => ['ns1']
+  }
+
+  # Reverse Zone
+  dns::zone { '1.168.192.IN-ADDR.ARPA':
+    soa         => 'ns1.setkeh.local',
+    soa_email   => 'setkeh@gmail.com',
+    nameservers => ['ns1']
+  }
+
+  # A Records:
+  dns::record::a {
+    'dns':
+      zone => 'setkeh.local',
+      data => ['192.168.1.254'];
+      ptr  => true;
+    'test':
+      zone => 'setkeh.local',
+      data => ['192.168.1.12];
+      ptr  => true;
+  }
--- site.pp
+++ site.pp
@@ -4,6 +4,10 @@ node 'arch-laptop.setkeh.local' {
 node '841734fa-cd43-4516-baa9-75e7a875d82e' {
   #puppet code
 }
+node 'dns.setkeh.local' {
+  #puppet code
+  include 'nodes/dns.pp'
+}
 
 # Site Wide Config
 import 'site/*.pp'
