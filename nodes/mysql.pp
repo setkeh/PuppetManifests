2e438b2dfad55df8eb1f3707b6ff30406bd0267f
Setkeh <setkeh@gmail.com>
2015-04-29 06:39:58 +1000

    added a correction

--- nodes/mysql.pp
+++ nodes/mysql.pp
@@ -3,7 +3,7 @@ include mysql
 class { '::mysql::server':
   root_password           => 'strongpassword',
   remove_default_accounts => true,
-  override_options        => $override_options
+  override_options        => $override_options,
 }
 
 mysql::db { 'DNS':
