74ac589b1d727f6e25c805ab74ab65aca060ea65
Setkeh <setkeh@gmail.com>
2015-04-29 01:39:51 +1000

    More improvements

--- nodes/dns.pp
+++ nodes/dns.pp
@@ -1,32 +1,10 @@
-class { 'bind':
-  confdir    => '/etc/named',
-  cachedir   => '/var/lib/named',
-  forwarders => [
-    '8.8.8.8',
-    '8.8.4.4',
-  ],
-  dnssec     => true,
-  version    => 'Controlled by Puppet',
-}
-
-bind::zone { 'setkeh.local':
-  zone_type       => 'master',
-  domain          => 'setkeh.local',
- #allow_updates   => [ 'key local-update', ],
- #allow_transfers => [ 'secondary-dns', ],
-  ns_notify       => true,
-  dnssec          => false,
-}
-
-resource_record { 'test.setkeh.local':
-  ensure  => present,
-  record  => 'test.setkeh.local',
-  type    => 'A',
-  data    => [ '192.168.1.12' ],
-  ttl     => 86400,
-  zone    => 'setkeh.local',
-  server  => 'ns.setkeh.local',
-  #keyname => 'local',
-  #hmac    => 'hmac-sha1',
-  #secret  => 'aLE5LA=='
+include powerdns
+class { 'powerdns::postgresql':
+  ensure   => present,
+  user     => 'powerdns',
+  password => 'secret',
+  host     => 'localhost',
+  port     => '5432',
+  dbname   => 'pdns',
+  dnssec   => 'yes',
 }
--- site.pp
+++ site.pp
@@ -6,18 +6,12 @@ node '841734fa-cd43-4516-baa9-75e7a875d82e' {
 }
 node 'dns.setkeh.local' {
   #puppet code
-#import 'nodes/dns.pp'
-include pdns
-pdns::nameserver {
-    backend           =>     'sqlite',
-    listen_address    =>     '192.168.1.254',
-    forward_domain    =>     'local',
-  }
-  pdns::resolver {
-    listen_address    =>      '127.0.0.1',
-    forward_domain    =>      'local',
-    nameservers       =>      '8.8.8.8,8.8.4.4',
-  }
+include powerdns
+powerdns::config { 'cache-ttl':
+  ensure => present,
+  value  => 20,
+}
+import 'nodes/dns.pp'
 }
 
 # Site Wide Config
